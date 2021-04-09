398d8079b18f0094ec43409c15e06d70302653a5
ReiketsuHen <81604033+ReiketsuHen@users.noreply.github.com>
2021-04-08 21:03:48 -0500

    Update sensores.py

--- sensores.py
+++ sensores.py
@@ -11,13 +11,13 @@ def distancia:
   GPIO.output(trig,FALSE)
   time.sleep(0.000005)
   GPIO.output(trig,TRUE)
-  time.sleep(0.000005)    #aprox 5us
+  time.sleep(0.000005)             #aprox 5us
   GPIO.output(trig,FALSE)
   while GPIO.input(echo)==0:
     time_start = time.time()
   while GPIO.input(echo)==1:
     time_end = time.time()
   time_dif = time_end - time_start
-  dis = (0.0343/2)*time_dif
+  dis = (34300/2)*time_dif        #(0.0343/2)*time_dif <- cm/us
   
   return dis
